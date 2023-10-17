77dd1517344a3486d6ccd0935fa3e43b150040dd
BARKA Oussama <62702326+0m3ga13@users.noreply.github.com>
2023-10-17 01:13:17 +0100

    fix(portal): fix the portal issue

--- src/components/portal/Portal.jsx
+++ src/components/portal/Portal.jsx
@@ -1,15 +1,19 @@
+"use client";
+
 import { createPortal } from "react-dom";
 
 const Portal = ({ children }) => {
-    const mount = document.body;
-    const el = document.createElement("div");
+    if (typeof window === "object") {
+        const mount = document.body;
+        const el = document.createElement("div");
 
-    // useEffect(() => {
-    //     mount.appendChild(el);
-    //     return () => mount.removeChild(el);
-    // }, [el, mount]);
+        // useEffect(() => {
+        //     mount.appendChild(el);
+        //     return () => mount.removeChild(el);
+        // }, [el, mount]);
 
-    return createPortal(children, mount);
+        return createPortal(children, mount);
+    }
 };
 
 export default Portal;
