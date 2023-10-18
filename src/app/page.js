36e65f9cd1ae1bd4342a52aae569016a1fef2ff1
BARKA Oussama <62702326+0m3ga13@users.noreply.github.com>
2023-10-18 20:49:40 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,6 @@
 import { doc, setDoc } from "firebase/firestore";
 
-import { db } from "@/lib/firebase";
-
 export default function Home() {
-    setDoc(doc(db, "categories", "LA"), {
-        name: "Los Angeles",
-        state: "CA",
-        country: "USA",
-    });
 
     return (
         <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
