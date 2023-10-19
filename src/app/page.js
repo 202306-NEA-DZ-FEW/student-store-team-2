15a604e571335cad61edf4d165d824058706e6cb
Bolphunga <aymendj@yahoo.com>
2023-10-19 16:54:06 +0200

    refactor(productcard): fixed styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,4 @@
-import { doc, setDoc } from "firebase/firestore";
-
-import { db } from "@/lib/firebase";
-
 export default function Home() {
-
     return (
         <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
     );
