d599c21d80fda8a86e4b9bb825a1d7ef3228659c
BARKA Oussama <62702326+0m3ga13@users.noreply.github.com>
2023-10-19 21:10:44 +0100

    refactor(prettier): fix home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,4 @@
-import { doc, setDoc } from "firebase/firestore";
-
 export default function Home() {
-
     return (
         <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
     );
