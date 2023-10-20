e18e637afe5705474fde0377653b5b1aeb799a28
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2023-10-20 18:08:47 +0100

    test(buton component): update test
    fix #14

--- src/components/buyborrow-btn/test/BuyBorrowBtn.test.js
+++
@@ -1,7 +0,0 @@
-import renderer from "react-test-renderer";
-import BuyBorrowBtn from "../BuyBorrowBtn";
-
-it("renders correctly", () => {
-    const tree = renderer.create(<BuyBorrowBtn />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
