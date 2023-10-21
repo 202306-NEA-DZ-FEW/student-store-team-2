56e5aea5497013727b40b8d37945fa80e95c054d
BARKA Oussama <62702326+0m3ga13@users.noreply.github.com>
2023-10-22 00:08:47 +0100

    fix(translation): fix the bugs

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,4 @@
+"use client";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
--- src/middleware.js
+++ src/middleware.js
@@ -5,7 +5,7 @@ export default createMiddleware({
     locales: ["en", "ar"],
 
     // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-    defaultLocale: "en",
+    defaultLocale: "ar",
 });
 
 export const config = {
