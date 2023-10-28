2517a870b532d1ec7b6fe0999e3a264e8c100c30
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2023-10-28 14:35:59 +0100

    test(product-detail-section): create test for product-detail-section

---
+++ src/components/product-display/ProductDetailSection/__test__/ProductDetailSection.test.js
@@ -0,0 +1,15 @@
+import renderer from "react-test-renderer";
+import { NextIntlClientProvider } from "next-intl";
+import messages from "../../../../../messages/en.json";
+import ProductDetailSection from "../ProductDetailSection";
+
+it("renders correctly", () => {
+    const tree = renderer
+        .create(
+            <NextIntlClientProvider locale='en' messages={messages}>
+                <ProductDetailSection />
+            </NextIntlClientProvider>
+        )
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
