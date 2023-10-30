1c98e9b327a9f27344abfd2dfbfdd775d15a81b7
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2023-10-30 17:22:45 +0100

    test(product-display): create tests for ProductDetailSection and profilePic componenets

--- src/components/profilePicture/ProfilePic.jsx
+++ src/components/profilePicture/ProfilePic.jsx
@@ -1,9 +1,12 @@
 function ProfilePic({ user }) {
-    const color = getRandomColor();
+    // const color = getRandomColor();
+
     return (
         <div
-            className='w-16 h-16 bg-gray-300 rounded-full flex items-center justify-center'
-            style={{ backgroundColor: color }}
+            className='w-16 h-16 bg-sky-200 rounded-full flex items-center justify-center drop-shadow'
+            style={{
+                backgroundColor: user ? `url(${user.profile_pic})` : "lavender",
+            }}
         >
             <span className='text-white text-2xl'>
                 {user ? user.name[0] : null}
@@ -13,11 +16,11 @@ function ProfilePic({ user }) {
 }
 export default ProfilePic;
 
-function getRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+// function getRandomColor() {
+//     const letters = "0123456789ABCDEF";
+//     let color = "#";
+//     for (let i = 0; i < 6; i++) {
+//         color += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+// }
