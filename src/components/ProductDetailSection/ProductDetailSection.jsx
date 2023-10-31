6da521347677ace16deea67322bcd3c8736302e9
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2023-10-31 13:28:41 +0100

    feat(product-display-section): add product category part to ProductDetailSection component

--- src/components/ProductDetailSection/ProductDetailSection.jsx
+++ src/components/ProductDetailSection/ProductDetailSection.jsx
@@ -5,18 +5,19 @@ import ProfileUserInfo from "../profileUserInformation/ProfileUserInfo";
 import BuyBorrowBtn from "../buyborrow-btn/BuyBorrowBtn";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ProductDetailSection({ user, productData }) {
     const t = useTranslations("Index");
     return (
-        <section className=' w-full flex flex-col'>
+        <section className=' ml-2 w-full flex flex-col'>
             <div className='w-1/2 m-4 flex gap-2'>
                 <ProfilePic user={user} />
                 <ProfileUserInfo user={user} />
             </div>
             <div className='border border-0.5 border[#EDF1F3] rounded-md drop-shadow-sm p-4 w-10/12'>
                 <div className='mb-10 ml-4'>
-                    <h2 className='font-lato text-5xl uppercase text-[#55585B]'>
+                    <h2 className='font-lato text-3xl uppercase text-[#55585B]'>
                         {productData
                             ? productData.name
                             : "Product Name Not Available"}
@@ -29,7 +30,7 @@ export default function ProductDetailSection({ user, productData }) {
                             width={139}
                             height={39}
                         ></Image>
-                        <div className='font-jost font-extrabold text-sm text-bkg absolute bottom-1 left-2 tracking-wider '>
+                        <div className='font-jost font-extrabold text-base text-bkg absolute bottom-1 left-2 tracking-wider '>
                             {t("State")}:{" "}
                             {productData
                                 ? productData.condition + "/10"
@@ -38,12 +39,12 @@ export default function ProductDetailSection({ user, productData }) {
                     </div>
                 </div>
 
-                <p className='w-5/6 text-base font-medium mb-2'>
+                <p className='w-5/6 text-xs font-medium mb-2 leading-5 tracking-wide'>
                     {productData
                         ? productData.description
                         : "Description Not Available"}
                 </p>
-                <div className='ml-8 text-[#3A3A3A] w-full flex items-center gap-10 my-1'>
+                <div className='ml-8 mt-8 text-[#3A3A3A] w-full flex items-center gap-10 my-1'>
                     <div className='w-[180px] flex flex-row '>
                         <BuyBorrowBtn label='Buy' />
                         <p className='font-lato text-lg font-semibold'>
@@ -65,6 +66,19 @@ export default function ProductDetailSection({ user, productData }) {
                             </span>
                         </p>
                     </div>
+                </div>
+                <div className=' w-full flex mt-8 gap-2'>
+                    <p className=' text-xs text-[#3A3A3A] font-semibold '>
+                        Category :
+                    </p>
+                    <Link
+                        href='/'
+                        className='font-normal text-[#72AEC8] underline hover:text-[#45adda] transform hover:scale-105 transition-transform duration-300 ease-in-out'
+                    >
+                        {productData
+                            ? productData.category
+                            : "Category Not Available"}{" "}
+                    </Link>
                 </div>
             </div>
         </section>
