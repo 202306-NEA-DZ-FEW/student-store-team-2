56899dd5b5aa83b7aa033373148ccc201194106a
BARKA Oussama <62702326+0m3ga13@users.noreply.github.com>
2023-11-07 20:02:13 +0100

    feat(add product page): fix the add product layout

--- src/components/addProduct/AddProductForm.jsx
+++ src/components/addProduct/AddProductForm.jsx
@@ -1,46 +1,132 @@
 "use client";
-import { useState } from "react";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const AddProductForm = ({ categories }) => {
     const [images, setImages] = useState([]);
 
     const handleImageChange = (e) => {
-        // Handle image upload logic here
-        // For simplicity, let's assume the selected images are stored in 'images' state
         setImages([...images, ...Array.from(e.target.files)]);
     };
 
-    return (
-        <div className='max-w-2xl mx-auto p-4'>
-            <div className='flex justify-between items-center mb-4'>
-                <div className='flex space-x-4'>
-                    {images.slice(0, 4).map((image, index) => (
-                        <div key={index} className='w-20 h-20 bg-gray-200'>
-                            {/* Display uploaded images */}
-                            <img
-                                src={URL.createObjectURL(image)}
-                                alt={`Image ${index + 1}`}
-                                className='w-full h-full object-cover'
-                            />
-                        </div>
-                    ))}
-                    <label
-                        htmlFor='image-upload'
-                        className='w-20 h-20 bg-gray-200 flex items-center justify-center cursor-pointer'
-                    >
-                        <span className='text-gray-500 text-sm'>Add Image</span>
-                        <input
-                            id='image-upload'
-                            type='file'
-                            className='hidden'
-                            accept='image/*'
-                            onChange={handleImageChange}
-                            multiple
-                        />
-                    </label>
+    let imageLayout;
+    switch (images.length) {
+        case 1:
+            imageLayout = (
+                <div className='w-full h-full bg-gray-200'>
+                    <Image
+                        width={500}
+                        height={500}
+                        src={URL.createObjectURL(images[0])}
+                        alt='Image 1'
+                        className='w-full h-full object-cover'
+                    />
                 </div>
+            );
+            break;
+        case 2:
+            imageLayout = images.map((image, index) => (
+                <div key={index} className='w-1/2 h-1/2 bg-gray-200'>
+                    <Image
+                        width={500}
+                        height={500}
+                        src={URL.createObjectURL(image)}
+                        alt={`Image ${index + 1}`}
+                        className='w-full h-full object-cover'
+                    />
+                </div>
+            ));
+            break;
+        case 3:
+            imageLayout = (
+                <>
+                    <div className='w-full h-1/2 bg-gray-200'>
+                        <Image
+                            width={500}
+                            height={500}
+                            src={URL.createObjectURL(images[0])}
+                            alt='Image 1'
+                            className='w-full h-full object-cover'
+                        />
+                    </div>
+                    <div className='w-1/2 h-1/2 bg-gray-200'>
+                        <Image
+                            width={500}
+                            height={500}
+                            src={URL.createObjectURL(images[1])}
+                            alt='Image 2'
+                            className='w-full h-full object-cover'
+                        />
+                    </div>
+                    <div className='w-1/2 h-1/2 bg-gray-200'>
+                        <Image
+                            width={500}
+                            height={500}
+                            src={URL.createObjectURL(images[2])}
+                            alt='Image 3'
+                            className='w-full h-full object-cover'
+                        />
+                    </div>
+                </>
+            );
+            break;
+        case 4:
+            imageLayout = (
+                <>
+                    <div className='w-3/4 h-1/2 bg-gray-200'>
+                        <Image
+                            width={500}
+                            height={500}
+                            src={URL.createObjectURL(images[0])}
+                            alt='Image 1'
+                            className='w-full h-full object-cover'
+                        />
+                    </div>
+                    <div className='w-1/4 h-1/2 flex flex-col'>
+                        {images.slice(1).map((image, index) => (
+                            <div
+                                key={index}
+                                className='w-full h-1/2 bg-gray-200'
+                            >
+                                <Image
+                                    width={500}
+                                    height={500}
+                                    src={URL.createObjectURL(image)}
+                                    alt={`Image ${index + 2}`}
+                                    className='w-full h-full object-cover'
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </>
+            );
+            break;
+        default:
+            imageLayout = (
+                <label
+                    htmlFor='image-upload'
+                    className='w-full h-96 bg-gray-200 flex items-center justify-center cursor-pointer'
+                >
+                    <span className='text-gray-500 text-sm'>Add Image</span>
+                    <input
+                        id='image-upload'
+                        type='file'
+                        className='hidden'
+                        accept='image/*'
+                        onChange={handleImageChange}
+                        multiple
+                    />
+                </label>
+            );
+            break;
+    }
+
+    return (
+        <div className=' mx-auto p-4 flex md:flex-row flex-col space-x-5'>
+            <div className='flex justify-between items-center mb-4 md:w-1/2'>
+                <div className='flex flex-wrap'>{imageLayout}</div>
             </div>
-            <div className='mb-4'>
+            <div className='mb-4 md:w-1/2'>
                 <div className='flex space-x-4 mb-4'>
                     <div className='w-1/2'>
                         <select className='block w-full border border-gray-300 rounded-md p-2'>
@@ -80,14 +166,14 @@ const AddProductForm = ({ categories }) => {
                         className='w-1/2 block border border-gray-300 rounded-md p-2'
                     />
                 </div>
-            </div>
-            <div className='flex justify-between'>
-                <button className='px-4 py-2 bg-blue-500 text-white rounded-md'>
-                    Add Image
-                </button>
-                <button className='px-4 py-2 bg-green-500 text-white rounded-md'>
-                    Save
-                </button>
+                <div className='flex justify-center'>
+                    <button className='px-4 py-2 bg-blue-500 text-white rounded-md'>
+                        Add Image
+                    </button>
+                    <button className='px-4 py-2 bg-green-500 text-white rounded-md'>
+                        Save
+                    </button>
+                </div>
             </div>
         </div>
     );
