499244f0865979dc6d4faa6b7ecc282a0337be79
Ahmed Djebnoune <138311944+Bolphunga@users.noreply.github.com>
2023-11-13 22:44:18 +0100

    fixed filter design

--- src/components/sortingControl/SortingControl.jsx
+++ src/components/sortingControl/SortingControl.jsx
@@ -5,18 +5,49 @@ import React from "react";
 const SortingControl = () => {
     const t = useTranslations("Index");
     return (
-        <div className='font-lato font-semibold text-lg text-titleContent sm:flex justify-center w-5/6 h-16 hidden'>
-            <div className='flex justify-between items-center text-center mb-4 w-1/2 '>
-                <div className=' hover:underline'>{t("All")}</div>
-                <div className=' hover:underline'>{t("Awaiting Pickup")}</div>
-                <div className=' hover:underline'>{t("Active")}</div>
-                <div className=' hover:underline'>{t("Completed")}</div>
+        <div className='font-lato font-semibold text-lg text-titleContent flex justify-center w-fit sm:w-full lg:w-3/4 sm:h-16'>
+            <div className='md:flex justify-center items-center text-center sm:px-4  mr-2 mb-4 w-1/2 hidden'>
+                <div className=' hover:underline mx-2'>{t("All")}</div>
+                <div className=' hover:underline mx-2'>{t("Pending")}</div>
+                <div className=' hover:underline mx-2'>{t("Active")}</div>
+                <div className=' hover:underline mx-2'>{t("Completed")}</div>
             </div>
-            <div className='ml-28 mb-4 sort-list flex items-center '>
-                <label className='sort-label pr-2  hover:underline'>
+            <div className='ml-1/10 mr-2 mb-4 sort-list md:hidden flex justify-between px-4 items-center'>
+                <label className='sort-label pr-2  hover:underline hidden'>
+                    {t("Filter")}
+                </label>
+                <select className='font-lato text-secondaryGray text-lg sort-select h-14 w-32 border-2 rounded-lg'>
+                    <option
+                        value='All'
+                        className='text-secondaryGray text-center hover:bg-accent hover:text-white h-16 w-44'
+                    >
+                        {t("All")}
+                    </option>
+                    <option
+                        value='Pending'
+                        className='text-secondaryGray text-center hover:bg-accent hover:text-white h-16 w-44'
+                    >
+                        {t("Pending")}
+                    </option>
+                    <option
+                        value='Active'
+                        className='text-secondaryGray text-center hover:bg-accent hover:text-white h-16 w-44'
+                    >
+                        {t("Active")}
+                    </option>
+                    <option
+                        value='Completed'
+                        className='text-secondaryGray text-center hover:bg-accent hover:text-white h-16 w-44'
+                    >
+                        {t("Completed")}
+                    </option>
+                </select>
+            </div>
+            <div className='ml-1/10 mb-4 sort-list flex px-4 items-center'>
+                <label className='sort-label pr-2 hidden hover:underline'>
                     {t("Sort")}
                 </label>
-                <select className='font-lato text-secondaryGray text-lg sort-select px-2 h-14 w-36 border-2 rounded-lg'>
+                <select className='font-lato text-secondaryGray text-lg sort-select h-14 w-36 border-2 rounded-lg'>
                     <option
                         value='newest'
                         className='text-secondaryGray text-center hover:bg-accent hover:text-white h-16 w-44'
