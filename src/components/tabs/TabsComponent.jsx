04236e123abcd3efcc5ec010a0c9fee5f614caca
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2023-11-14 13:55:14 +0100

    feat(tabs components): recompose the TabsComponent into subcomponents AdditionalInfoTab, LocationTab

--- src/components/tabs/TabsComponent.jsx
+++ src/components/tabs/TabsComponent.jsx
@@ -1,74 +1,36 @@
 "use client";
-import React, { useRef, useEffect, useState } from "react";
+import { useState } from "react";
+import AdditionalInfoTab from "./additionalInfoTab/AdditionalInfoTab";
+import LocationTab from "./locationTab/LocationTab";
 
-function TabsComponent({ items }) {
-    const [selectedSection, setSelectedSection] = useState(0);
-    const [selectedTitle, setSelectedTitle] = useState(0);
-    const defaultBtnRef = useRef();
+function TabsComponent({ tabs }) {
+    const [selectedTabIndex, setSelectedTabIndex] = useState(0);
 
-    useEffect(() => {
-        if (defaultBtnRef.current) {
-            defaultBtnRef.current.focus();
-        }
-    }, []);
+    const components = [
+        <AdditionalInfoTab key={0} items={tabs} />,
+        <LocationTab key={1} items={tabs} />,
+    ];
+
+    const handleTabClick = (index) => {
+        setSelectedTabIndex(index);
+    };
 
     return (
-        <div className='flex justify-center items-center py-8'>
-            <div className='w-3/4 flex flex-col justify-start gap-y-6 '>
-                <div className='flex items-center font-medium text-gray-700 text-sm border-b border-gray-200'>
-                    {items
-                        ? items.map((section, sectionIndex) =>
-                              section.title.map((title, titleIndex) => (
-                                  <button
-                                      key={`${sectionIndex}.${titleIndex}`}
-                                      ref={
-                                          titleIndex === 0 && sectionIndex === 0
-                                              ? defaultBtnRef
-                                              : null
-                                      }
-                                      className='px-4 h-full rounded-sm uppercase text-[#55585B] hover:bg-[#89ceecba] hover:text-white hover:rounded-sm hover:text-base focus:drop-shadow-md focus:bg-[#72adc7ba] focus:text-white focus:rounded-sm focus:border-2 focus:border-gray-200 focus:text-semibold'
-                                      onClick={() => {
-                                          setSelectedSection(sectionIndex);
-                                          setSelectedTitle(titleIndex);
-                                      }}
-                                  >
-                                      {title}
-                                  </button>
-                              ))
-                          )
-                        : null}
-                </div>
-                <div className='bg-[#EDF1F3] p-1 m-2 rounded flex items-start text-sm font-base text-gray-700 '>
-                    {items
-                        ? items.map((item, index) => (
-                              <div
-                                  key={index}
-                                  className={`${
-                                      selectedSection === index
-                                          ? "border-2 border-gray-200 rounded-lg p-4"
-                                          : "hidden"
-                                  }`}
-                              >
-                                  <h1 className='text-3xl text-[#72adc7]'>
-                                      {item.content.title}
-                                  </h1>
-                                  <p className='text-[#3A3A3A] mt-8'>
-                                      {item.content.text}
-                                  </p>
-                                  {selectedSection === index &&
-                                      selectedTitle === 1 && (
-                                          <div
-                                              key={`${index + 1}`}
-                                              className='border-2 border-gray-200 rounded-lg p-4'
-                                          >
-                                              {item.location}
-                                          </div>
-                                      )}
-                              </div>
-                          ))
-                        : null}
-                </div>
+        <div className='w-3/4 flex flex-col justify-center items-center py-8'>
+            <div className='flex items-center font-medium text-gray-700 text-sm border-b border-gray-200'>
+                {tabs
+                    ? tabs.map((tab, index) => (
+                          <button
+                              key={index}
+                              onClick={() => handleTabClick(index)}
+                              className='px-4 h-full rounded-sm uppercase text-[#55585B] hover:bg-[#89ceecba] hover:text-white hover:rounded-sm hover:text-base focus:drop-shadow-md focus:bg-[#72adc7ba] focus:text-white focus:rounded-sm focus:border-2 focus:border-gray-200 focus:text-semibold'
+                          >
+                              {tab.title}
+                          </button>
+                      ))
+                    : null}
             </div>
+            <div className='w-full'>{components[selectedTabIndex]}</div>
         </div>
     );
 }
