b70d6c8daef016e62a18002a4a4daac9ea06893d
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2023-11-15 08:29:24 +0100

    test(tabs subcomponents): create tests for the subcomponents

--- src/components/tabs/locationTab/LocationTab.jsx
+++ src/components/tabs/locationTab/LocationTab.jsx
@@ -6,7 +6,7 @@ function LocationTab({ items }) {
             <div className='w-full flex flex-col justify-center gap-y-6 '>
                 <div className='bg-[#EDF1F3] p-1 m-2 rounded flex items-center text-sm font-base text-gray-700 '>
                     <div className='w-full border-2 border-gray-200 rounded-lg p-4'>
-                        {items[1].location}
+                        {items ? items[1].location : null}
                     </div>
                 </div>
             </div>
