0f1b1eee54dddb6914a0067e936656708cc43a06
Imane BELAID <31738219+Emybel@users.noreply.github.com>
2023-11-17 17:40:58 +0100

    feat(single product page): fetch data

--- src/components/product-display/__test__/ProductDisplay.test.js
+++ src/components/product-display/__test__/ProductDisplay.test.js
@@ -4,10 +4,34 @@ import messages from "../../../../messages/en.json";
 import ProductDisplay from "../ProductDisplay";
 
 it("renders correctly", () => {
+    const product = {
+        pid: 5,
+
+        category: 1,
+        condition: 8,
+        created_at: "2023-09-05",
+        description: "Classic literature set for book enthusiasts.",
+        for_borrow: true,
+        for_sale: false,
+        index: 0,
+        id: "product0",
+        image: [
+            "https://images.placeholders.dev/?width=100&height=100",
+            "https://images.placeholders.dev/?width=100&height=100",
+            "https://images.placeholders.dev/?width=100&height=100",
+            "https://images.placeholders.dev/?width=100&height=100",
+        ],
+        is_borrowed: false,
+        is_borrowed_period: 0,
+        is_sold: false,
+        name: "Classic literature set",
+        price: 99.95,
+        uid: "user123",
+    };
     const tree = renderer
         .create(
             <NextIntlClientProvider locale='en' messages={messages}>
-                <ProductDisplay />
+                <ProductDisplay product={product} />
             </NextIntlClientProvider>
         )
         .toJSON();
--- src/components/productDetailsSection/ProductDetailSection.jsx
+++ src/components/productDetailsSection/ProductDetailSection.jsx
@@ -8,8 +8,10 @@ import Image from "next/image";
 import Link from "next/link";
 import ContactDetails from "../contactDetails/ContactDetails/ContactDetails";
 
-export default function ProductDetailSection({ user, productData }) {
+export default function ProductDetailSection({ user, product, categories }) {
     const t = useTranslations("Index");
+
+    console.log("product", product);
     return (
         <section className='w-full sm:w-full flex flex-col'>
             <div className='w-full m-4 flex gap-2'>
@@ -19,9 +21,7 @@ export default function ProductDetailSection({ user, productData }) {
             <div className='border border-0.5 border[#EDF1F3] rounded-md drop-shadow-sm p-4 w-full'>
                 <div className='mb-10 ml-4'>
                     <h2 className='font-lato text-lg sm:3xl uppercase text-[#55585B]'>
-                        {productData
-                            ? productData.name
-                            : "Product Name Not Available"}
+                        {product && product.name}
                     </h2>
                     <div className='relative mt-2'>
                         <Image
@@ -32,36 +32,25 @@ export default function ProductDetailSection({ user, productData }) {
                             height={39}
                         ></Image>
                         <div className='font-jost font-extrabold text-sm sm:text-base text-bkg absolute bottom-1 left-2 tracking-wider '>
-                            {t("State")}:{" "}
-                            {productData
-                                ? productData.condition + "/10"
-                                : "Condition Not Available"}
+                            {t("State")}: {product && product.condition + "/10"}
                         </div>
                     </div>
                 </div>
 
                 <p className='w-full sm:w-5/6 text-sm sm:text-base font-medium mb-2 leading-5 tracking-wide'>
-                    {productData
-                        ? productData.description
-                        : "Description Not Available"}
+                    {product && product.description}
                 </p>
                 <div className=' mt-8 text-[#3A3A3A] w-full flex flex-col items-start sm:flex-row sm:items-center my-1'>
                     <div className=' flex flex-row gap-2 w-full sm:w-2/3 '>
                         <BuyBorrowBtn label='Buy' />
                         <p className='font-lato text-lg font-semibold'>
-                            $
-                            {productData
-                                ? productData.price.sell_price
-                                : " Sell Price Not Available"}
+                            ${/* {product && product.price.sell_price} */}
                         </p>
                     </div>
                     <div className='flex flex-row gap-2 sm:gap-0 w-full sm:w-2/3 mt-2'>
                         <BuyBorrowBtn label='Borrow' />
                         <p className='font-lato text-lg font-semibold '>
-                            $
-                            {productData
-                                ? productData.price.borrow_price
-                                : "Borrow Price Not Available"}
+                            ${/* {product && product.price.borrow_price} */}
                             <span className='text-[#55585B] text-sm font-thin font-jost pl-2'>
                                 per day
                             </span>
@@ -76,9 +65,7 @@ export default function ProductDetailSection({ user, productData }) {
                         href='/'
                         className='font-normal text-[#72AEC8] underline hover:text-[#45adda] text-sm sm:text-base transform hover:scale-105 transition-transform duration-300 ease-in-out'
                     >
-                        {productData
-                            ? productData.category
-                            : "Category Not Available"}{" "}
+                        {categories && categories.category_name}
                     </Link>
                 </div>
                 <ContactDetails user={user} />
