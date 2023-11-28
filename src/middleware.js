70c01e531b5dbe4fa46434bb8d5e2058abfaab09
BARKA Oussama <62702326+0m3ga13@users.noreply.github.com>
2023-11-29 00:21:20 +0100

    fix the middleware issues

--- src/middleware.js
+++ src/middleware.js
@@ -1,36 +1,25 @@
-/**
- * The above code exports a middleware function that combines Supabase middleware and
- * internationalization middleware for a Next.js application.
- * @param request - The `request` parameter is the incoming HTTP request object. It contains
- * information about the request, such as the request method, headers, URL, and body. It is used to
- * process and handle the request in the middleware function.
- * @returns The middleware function is returning the response from the `intlMiddleware` function.
- */
-//middleware.js
 import createMiddleware from "next-intl/middleware";
 
 import { supabseMiddleware } from "./utils/supabase/middleware";
 
-const intlMiddleware = createMiddleware({
-    locales: ["en", "ar"],
-    localeDetection: true,
-    defaultLocale: "en",
-});
-
 /**
- * The `supabseMiddleware` function is a middleware function that handles authentication and cookie
- * management for Supabase in a Next.js server-side rendering (SSR) environment.
- * @param request - The `request` parameter represents the incoming HTTP request object. It contains
- * information about the request, such as headers, cookies, and query parameters.
- * @param response - The `response` parameter is the HTTP response object that will be sent back to the
- * client. It is used to set cookies and modify the response headers.
- * @returns The `supabseMiddleware` function returns the `response` object.
+ * The function is a middleware that handles internationalization and authentication using Supabase.
+ * @param request - The `request` parameter is the incoming HTTP request object. It contains
+ * information about the request such as the HTTP method, headers, URL, and body.
+ * @returns The `res` variable is being returned.
  */
-
 export async function middleware(request) {
+    const intlMiddleware = createMiddleware({
+        locales: ["en", "ar"],
+        localeDetection: true,
+        defaultLocale: "en",
+    });
+
     const res = await intlMiddleware(request);
-    const response = await supabseMiddleware(request, res);
-    return response;
+    const supabase = await supabseMiddleware({ request, res });
+    await supabase.auth.getSession();
+
+    return res;
 }
 
 export const config = {
