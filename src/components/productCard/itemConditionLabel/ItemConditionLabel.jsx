2b18a6f55e33b24380b43f565218e9c42d3071b7
med <mohammed.bennaceur98@gmail.com>
2023-11-28 13:00:39 +0100

    fix(component): fix product card design

--- src/components/productCard/itemConditionLabel/ItemConditionLabel.jsx
+++ src/components/productCard/itemConditionLabel/ItemConditionLabel.jsx
@@ -14,8 +14,8 @@ const ItemConditionLabel = ({ itemCondition }) => {
                 width={139}
                 height={39}
             ></Image>
-            <div className='font-jost font-extrabold text-sm text-bkg absolute bottom-1 left-2'>
-                {t("State")}: {itemCondition}
+            <div className='font-jost font-extrabold text-sm text-bkg absolute bottom-1 left-2 w-fit'>
+                {itemCondition}$
             </div>
         </div>
     );
